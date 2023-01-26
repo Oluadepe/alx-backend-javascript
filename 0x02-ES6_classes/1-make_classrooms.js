d327ec8703e33d84f47658ef3d23efab98e2b782
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-26 19:33:37 +0000

    ClassRoom imported, initializeRoom function that return 3 array implemented

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,8 @@
-import ClassRoom from "./0-ClassRoom.js"
+import ClassRoom from "./0-classroom.js"
 
 export default function initializeRooms() {
-   const cr1 = new ClassRoom(19);
-   const cr2 = new ClassRoom(20);
-   const cr3 = new ClassRoom(34);
-
-   return [cr1, cr2, cr3];
+  let cr1 = new ClassRoom(19);
+  let cr2 = new ClassRoom(20);
+  let cr3 = new ClassRoom(34);
+  return [cr1, cr2, cr3];
 }
