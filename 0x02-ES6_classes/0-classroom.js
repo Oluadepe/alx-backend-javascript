b4ccc9112e094acaf21171205786af6e2bebebb0
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-26 18:48:21 +0000

    Classroom Ininitiated with one attribute

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/* 
+class named "Class" implmented with one attribute named "maxStudentsSize (Number)" and assigned to "_maxStudentsSize"
+	
+*/
+class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
+
+export default ClassRoom;
