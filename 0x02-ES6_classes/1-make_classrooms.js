c8b5e378638b72317f4d2646abe54e8e6b2d26f7
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-26 21:33:00 +0000

    Implemented HolbertonCourse Class with construstor taking 3 attributes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,12 @@
-import ClassRoom from "./0-classroom.js"
+import Classroom from './0-classroom';
 
 function initializeRooms() {
-  let c1 = new ClassRoom(19);
-  let c2 = new ClassRoom(20);
-  let c3 = new ClassRoom(34);
-  return [c1, c2, c3];
+  
+    let c1 = new Classroom(19),
+    let c2 = new Classroom(20),
+    let c3 = new Classroom(34),
+    
+    return [c1, c2, c3];
 }
 
 export default initializeRooms;
