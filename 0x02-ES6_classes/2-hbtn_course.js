e23f0414e371965eeae89139a0f85131c6aeef46
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-26 21:18:36 +0000

    Implemented HolbertonCourse Class with construstor taking 3 attributes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,52 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name;
-        this._length = length;
-        this._students = students;
-    }
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-    set name(name) {
-        if (typeof value !== "string") {
-            throw new TypeError("name must be a string");
-        }
-        this._name = name;
+  /**
+   * @param {String} name
+   */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    get name() {
-      return this.name;
-	}
+    this._name = name;
+  }
 
-    set length(length) {
-        if (typeof value !== "number") {
-            throw new TypeError("length must be a number");
-        }
-        this._length = length;
+  get name() {
+    return this._name;
+  }
+
+  /**
+   * @param {Number} length
+   */
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-    get length() {
-      return this.length;
-        }
+    this._length = length;
+  }
 
-    set students(students) {
-        if (!Array.isArray(value)) {
-            throw new TypeError("students must be an array of strings");
-        }
-        this._students = student;
+  get length() {
+    return this._length;
+  }
+
+  /**
+   * @param {Array} students
+   */
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an Array');
     }
-    get students() {
-      return this.students;
-        }
+  }
 
+  get students() {
+    return this._students;
+  }
 }
 
 export default HolbertonCourse;
-
