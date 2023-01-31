00b0e9e3313814b13acba0a8d5269befddbb41de
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-01-31 23:55:46 +0000

    getListStudentIds returns an array of ids

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,6 @@
-export default function getListStudents(students) {
-	  if (!Array.isArray(students)) {
-		      return [];
+export default function getListStudentIds(arrObj) {
+	  if (Array.isArray(arrObj)) {
+		      return arrObj.map((obj) => obj.id);
 		    }
-
-	  const arr = [
-		      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-		      { id: 2, firstName: 'James', location: 'Columbia' },
-		      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-		    ];
-
-	  return arr;
+	  return [];
 }
-
