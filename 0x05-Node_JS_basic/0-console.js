1aa3bc03328ca186bfbfe8a9664f54737494035e
Olusegun Mayungbe <Oluwaseun001@gmail.com>
2023-03-22 04:48:10 +0000

    fixed

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
  */
-function displayMessage(message) {
-	  console.log(message);
+function displayMessage("Hello NodeJS!") {
+	  console.log("Hello NodeJS!");
 }
 
 module.exports = displayMessage;
