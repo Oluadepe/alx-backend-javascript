b9589002449f2446be06d52c3c98fe0d5a522820
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-15 05:23:46 +0100

    Applied a fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,9 +1,6 @@
 const express = require('express');
-
-const args = process.argv.slice(2);
-const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
+const fs = require('fs');
+const csv = require('csv-parser');
 
 const app = express();
 const port = 1245;
@@ -12,17 +9,34 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
-  try {
-    const students = await countStudents(DATABASE);
-    res.send(`${msg}${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${msg}${error.message}`);
-  }
+app.get('/students', (req, res) => {
+  const dbName = 'database.csv';
+  let students = [];
+  let csStudents = [];
+  let sweStudents = [];
+
+  fs.createReadStream(dbName)
+    .pipe(csv())
+    .on('data', (data) => {
+      students.push(data);
+      if (data.field === 'CS') {
+        csStudents.push(data.firstname);
+      } else if (data.field === 'SWE') {
+        sweStudents.push(data.firstname);
+      }
+    })
+    .on('end', () => {
+      const csCount = csStudents.length;
+      const sweCount = sweStudents.length;
+
+      const response = `This is the list of our students\nNumber of students: ${students.length}\nNumber of students in CS: ${csCount}. List: ${csStudents.join(', ')}\nNumber of students in SWE: ${sweCount}. List: ${sweStudents.join(', ')}`;
+      res.send(response);
+    });
 });
 
 app.listen(port, () => {
+  console.log(`HTTP server is running on port ${port}`);
 });
 
 module.exports = app;
+
