6ea054225e11470e97ba0b5c01c6844ab3d84b80
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-04-16 21:07:10 +0100

    Added index file to link the application and student route

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,12 @@
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+// link route to application and student 
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
